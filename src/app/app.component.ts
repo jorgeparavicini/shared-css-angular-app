c6d5e52361e18fd2c439612fca96cf4ac108fa5a
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-06-09 10:38:48 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import '../../../stencil-components';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shared-css-app';
+}
